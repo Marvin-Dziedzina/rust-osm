2d618b05d5f08dfe30b9fb8809e2a476c2450585
Marvin Dziedzina <marvin.dziedzina@protonmail.com>
2025-08-09 19:24:04 +0200

    Added feature validity check for coordinate_f32 and coordinate_f64

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,16 @@
 #![warn(unused)]
 #![warn(nonstandard_style)]
 #![warn(future_incompatible)]
-#![warn(rustdoc::missing_doc_code_examples)]
 
 //! A OSM API wrapper.
+
+// Ensure that one of `coordinate_f32` or `coordinate_f64` is enabled.
+#[cfg(not(any(feature = "coordinate_f32", feature = "coordinate_f64")))]
+compile_error!("One of `coordinate_f32` or `coordinate_f64` must be enabled.");
+
+// Ensure that `coordinate_f32` and `coordinate_f64` are never enabled together.
+#[cfg(all(feature = "coordinate_f32", feature = "coordinate_f64"))]
+compile_error!("Features `coordinate_f32` and `coordinate_f64` can not be enabled together.");
 
 pub mod coord;
 pub mod rest_methods;
